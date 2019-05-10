eddb50d34d54351e797153e28609a548be4ec42e
illia <illia.ohrimenko@gmail.com>
2019-05-10 23:22:11 +0300

    initial commit

---
+++ src/components/hoc/index.js
@@ -0,0 +1,4 @@
+import withData from './with-data';
+import withBookstoreService from './with-bookstore-service';
+
+export { withData, withBookstoreService };
---
+++ src/services/bookstore-service.js
@@ -0,0 +1,40 @@
+export default class BookstoreService {
+  data = [
+    {
+      id: 1,
+      title: 'Production-Ready Microservices',
+      author: 'Susan J. Fowler',
+      price: 32,
+      coverImage:
+        'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg'
+    },
+    {
+      id: 2,
+      title: 'Eloquent JavaScript',
+      author: 'Marijn Haverbeke',
+      price: 32,
+      coverImage:
+        'https://images-na.ssl-images-amazon.com/images/I/51I9naPg55L._SX376_BO1,204,203,200_.jpg'
+    },
+    {
+      id: 3,
+      title: 'JavaScript : The Good Parts',
+      author: 'Douglas Crockford',
+      price: 32,
+      coverImage:
+        'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg'
+    }
+  ];
+
+  getBooks() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() < 0.75) {
+          resolve(this.data);
+        } else {
+          reject(new Error('Outh!'));
+        }
+      }, 700);
+    });
+  }
+}
